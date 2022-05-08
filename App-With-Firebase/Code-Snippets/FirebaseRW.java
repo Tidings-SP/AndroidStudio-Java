b1bcd2de40ab5decd7cbb211bf81d40a935682c1
SuryaPrakash <76809211+Tidings-SP@users.noreply.github.com>
2022-05-08 12:47:13 +0530

    Update FirebaseRW.java

--- App-With-Firebase/Code-Snippets/FirebaseRW.java
+++ App-With-Firebase/Code-Snippets/FirebaseRW.java
@@ -1,1 +1,20 @@
+private void setValueOnChange(DatabaseReference databaseReference, TextView textView) {
+        // Look for the data change(read)
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // This method is called whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                textView.setText(value);
 
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", databaseError.toException());
+            }
+        });
+}
+
+DatabaseReference.setValue(//Enter the value to be written in firebase database);
